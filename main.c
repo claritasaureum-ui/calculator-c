f451a1f676d90c29d7896e6248f6a8cfe5e96dc4
João Pedro <joao.pedrosimoesss@gmail.com>
2026-07-13 18:13:51 -0300

    adding the subsctraction function

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
+
+int sub(int a, int b){
+    return a - b;
+}
+
 int sum(int a, int b) {
     return a + b;
 }
 
 int main() {
-    int num1, num2, result;
+    int num1, num2, result, op;
     
     printf("Enter first number: ");
     scanf("%d", &num1);
@@ -13,7 +18,17 @@ int main() {
     printf("Enter second number: ");
     scanf("%d", &num2);
     
-    result = sum(num1, num2);
+
+    printf("0 - sum\n1 - sub\nwhats the operation?:");
+    scanf("%d",&op);
+
+    if(op){
+        result = sub(num1, num2);
+
+    } else{
+        result = sum(num1, num2);
+
+    };
     
     printf("Sum: %d\n", result);
     
